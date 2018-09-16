113dfd70704b52cc2b7fff9b5af82a67a14d414f
assel <aslhdyt@gmail.com>
2018-09-16 10:55:18 +0700

    added test get Team

--- app/src/test/java/com/assel/footbalapp/ApiRepoTest.kt
+++ app/src/test/java/com/assel/footbalapp/ApiRepoTest.kt
@@ -1,32 +1,30 @@
 package com.assel.footbalapp
 
-import com.assel.footbalapp.model.Events
 import com.assel.footbalapp.restApi.Endpoint
 import com.assel.footbalapp.restApi.RestClient
 import org.junit.Before
 import org.junit.Test
-import retrofit2.Call
 import java.util.*
 
 class ApiRepoTest {
 
-    lateinit var call: Call<Events>
-
-    var isNextEvent: Boolean = Random().nextBoolean()
+    private val client = RestClient.getInstance().create(Endpoint::class.java)
 
     @Before
     fun setUp() {
-        call = {
-            val client = RestClient.getInstance().create(Endpoint::class.java)
-            if (isNextEvent) client.nextEventLeague()
-            else client.lastEventLeague()
-        } ()
     }
     @Test
-    fun testEmptyList() {
-        val result = call.execute().body()?.events
+    fun testGetEventAndTeam() {
+        val callEvents = {
+            if (Random().nextBoolean()) client.nextEventLeague()
+            else client.lastEventLeague()
+        } ()
+        val eventResult = callEvents.execute().body()?.events
 
-        println("result size: ${result?.size}, isNextEvent: $isNextEvent")
-        assert(result?.size!! > 0)
+        assert(eventResult?.size ?: -1 > 0)
+        val randomEvent = eventResult?.get(Random().nextInt(eventResult.lastIndex))
+        val callTeam = client.getTeamDetailsById(randomEvent?.idAwayTeam ?: 0)
+        val teamResult = callTeam.execute().body()?.teams?.get(0)
+        assert(randomEvent?.idAwayTeam == teamResult?.idTeam?.toInt())
     }
 }
